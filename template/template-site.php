009d2ea0be93b8d99a62c40bfc49a09e7895e0d3
Buldii <krystianbulanda83@gmail.com>
2022-11-15 21:01:20 +0100

    update

--- template/template-site.php
+++ template/template-site.php
@@ -10,7 +10,31 @@
 </head>
 <body>
     <?php include '../header_footer/header.php' ?>
-
+    <div class="container">
+        <?php
+        $username = "root";
+        $password = "";
+        $host = "localhost";
+        $db_name = "kino_kodzik";
+        $db = new mysqli($host, $username, $password, $db_name);
+        $sql = "SELECT id, nazwa, plakat FROM filmy LIMIT 10"
+        $result = $db->query($sql);
+        $films = '';
+        while ($row = $result->fetch_assoc()) {
+            $films .= 
+            "
+            <a href="."TODO DOADAĆ LINK".">
+                <img src=". $row['link'] ." alt="">
+                <h5>". $row['nazwa'] ."</h5>
+            </a>
+            ";
+        }
+        ?>
+        <div>
+            <img src="" alt="">
+            <h5></h5>
+        </div>
+    </div>
     <?php include '../header_footer/footer.php' ?>
 </body>
 </html>
